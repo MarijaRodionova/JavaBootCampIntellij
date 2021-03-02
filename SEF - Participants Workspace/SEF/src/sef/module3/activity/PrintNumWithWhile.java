d102c9632bc8c6b41167254575634cfe3dfd408a
Marija Rodionova <marija.limare@gmail.com>
2021-03-02 11:56:57 +0200

    commit assignmentt

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/PrintNumWithWhile.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/PrintNumWithWhile.java
@@ -16,14 +16,19 @@ public class PrintNumWithWhile {
 		
 		
 		// Print all even numbers less than 100
-		int i = 0;
+		int i = 2;
 		while(i<100){
 			System.out.println(i);
-			i=i+2;			
+			i=i+2;
 		}
 		
 		 
 		// write code to Print all odd numbers less than 100
+		int a = 1;
+		while(a < 100){
+			System.out.println(a);
+			a=a+2;
+		}
 
 	}
 
