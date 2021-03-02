86d56169f395c87785fb9afbcdff743da80912e5
Marija Rodionova <marija.limare@gmail.com>
2021-03-02 10:05:41 +0200

    My first commit

---
+++ SEF - Participants Workspace/SEF/src/sef/module3/sample/sef/module2/sample/HelloWorld.java
@@ -0,0 +1,8 @@
+package sef.module2.sample;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- SEF - Participants Workspace/SEF/src/sef/module3/sef/module3/activity/VariableAssignmentActivity.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/sef/module3/activity/VariableAssignmentActivity.java
@@ -13,6 +13,8 @@ public class VariableAssignmentActivity {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int myNumber = 5;
+		System.out.println("My number is " + myNumber);
 		
 		// 1- Declare a variable of type int and assign it default value.
 		// 2- Update the value 
