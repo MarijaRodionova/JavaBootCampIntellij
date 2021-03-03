86c193a8997b773f5919a2e7f0f2c7f2a2b7b189
Marija Rodionova <marija.limare@gmail.com>
2021-03-03 15:22:40 +0200

    Shape activity

--- SEF - Participants Workspace/SEF/src/sef/module8/activity/TryCatchFinallyActivity.java
+++ SEF - Participants Workspace/SEF/src/sef/module8/activity/TryCatchFinallyActivity.java
@@ -1,4 +1,4 @@
-package sef.module8.activity;
+package activity;
 //Needs to be completed
 //The following program, when run, results in an exception.
 //1 - Use try catch appropriately to make sure that the exception is caught and a user friendly message is displayed. 
@@ -16,6 +16,14 @@ void catchMeIfYouCan()
 	for(int i=0;i<=10;i++)
 	{
 		System.out.println(arr[i]);
+	}
+
+	String str = null;
+
+	try {
+		System.out.println(str.toUpperCase());
+	} catch (NullPointerException ex) {
+		System.out.println ("Not initialized " + ex.getMessage());
 	}
 	
 	
