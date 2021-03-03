0527df3e76e614b02bc48ee6435a01e5b751ac6d
Marija Rodionova <marija.limare@gmail.com>
2021-03-03 10:48:42 +0200

    commit assignment

--- SEF - Participants Workspace/SEF/src/sef/module6/activity/Employee_I.java
+++ SEF - Participants Workspace/SEF/src/sef/module6/activity/Employee_I.java
@@ -1,24 +1,39 @@
-package sef.module6.activity;
+package activity;
 
 // class Employee_I must extend class Person_I. Complete code for the same
-public class Employee_I  {
-	
+public class Employee_I {
+
 	//Attributes
 	private double salary;
-	private String title;
-	
+	private int title;
+
 	//Behavior - write default constructor. Print 'I'm an Employee_I Constructor'
-	
-	
-	// write getter for int salary
-	
-	
-	// write setter for int salary
-	
-	
-	// write getter for String title
-	
-	
-	// write setter for String title
-	
-}
+	public Employee_I() {
+		System.out.println("I'm an Employee constructor");
+
+
+			// write getter for int salary
+			public double getSalary() {
+				return salary;
+			}
+
+
+			// write setter for int salary
+			public void setSalary (String salary) {
+				this.salary = salary;
+
+
+				// write getter for String title
+				public int getTitle () {
+					return title;
+				}
+
+				// write setter for String title
+				public void setTitle ( int title) {
+					this.title = title;
+				}
+
+			}
+		}
+	}
+
