7e1a870914d1b83be2ca2ff8e233b2d15567c9f8
Marija Rodionova <marija.limare@gmail.com>
2021-03-05 14:53:50 +0200

    activity

--- SEF - Participants Workspace/SEF/src/sef/module17/activity/EmployeeJDBC.java
+++ SEF - Participants Workspace/SEF/src/sef/module17/activity/EmployeeJDBC.java
@@ -1,5 +1,7 @@
-package sef.module17.activity;
+package activity;
 //Needs to be completed
+import org.junit.Assert;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,8 +10,24 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class EmployeeJDBC {
+//	Employee emp  = new Employee();
 
-	public Connection createConnection()
+	public static void main(String[] args) {
+		Employee employee = findEmployeeById(1);
+		System.out.println(employee.getId());
+		System.out.println(employee.getFirstName());
+		System.out.println(employee.getLastName());
+		System.out.println(employee.getSalary());
+		Employee employee1 = new Employee(2,"John","Doe",200);
+		insertEmployee(employee1);
+//		Connection con = createConnection();
+//		Employee e1 = new Employee();
+//		e1 = findEmployeeById(2);
+//		Assert.assertEquals("Marija", e1.getFirstName());
+//		System.out.println(e1.getFirstName());
+	}
+
+	public static Connection createConnection()
 	{
 		Connection con=null;
 		String url = "jdbc:mysql://localhost/activity";
@@ -31,21 +49,30 @@ public class EmployeeJDBC {
 		return con;
 	}
 	
-	public Employee findEmployeeById(String id)
+	public static Employee findEmployeeById(int id)
 	{
 		Connection con = createConnection();
 		Employee emp=null;
 		try {
 		// 1 - Create a PreparedStatement with a query
-		
+		PreparedStatement pStmt = con.prepareStatement("select * employee where id = ? and salary = ?");
+			statement.setInt(1, id);
 
 		// 2 - Search for the given id
-		
+			ResultSet rs = statement.executeQuery();
+
 
 		// 3 - Execute this query
+//		ResultSet rs = pStmt.executeQuery();
 		
-		
-		// 4 - If resultset is not null, then initialize emp object with data 
+		// 4 - If resultset is not null, then initialize emp object with data
+			if(rs.next()) {
+				emp = new Employee();
+				emp.setId(rs.getInt(1));
+				emp.setFirstName(rs.getString(2));
+				emp.setLastName(rs.getString(3));
+				emp.setSalary(rs.getInt(4));
+			}
 		
 		con.close();
 		} catch (SQLException e) {
@@ -57,21 +84,34 @@ public class EmployeeJDBC {
 	}
 
 	public ArrayList<Employee> findEmployeesByName(String name)
-	{
+
+//	public static Employee insertEmployee(Employee emp)
+//	{
 		Connection con = createConnection();
-		ArrayList<Employee> list = new ArrayList<Employee>();
-		
+	public ArrayList<Employee> findEmployeesByName(int name)
+
 		try {
 		// 1 - Create a PreparedStatement with a query
-		
+			con.setAutoCommit(false);
+			PreparedStatement statement = con.prepareStatement("select * employee where name = ?");
+			statement.setString(1, emp.getFirstName());
+			statement.setString(2, emp.getLastName());
+			statement.setInt(3, emp.getSalary());
+			int rs = statement.executeUpdate();
 
 		// 2 - Search for the given id
-		
 		// 3 - Execute this query
+
 		
 		
-		// 4 - While there are some records, continue 
+		// 4 - While there are some records, continue
+			if(rs==1) {
+				System.out.println("Succesful inserted value");
+			} else {
+				System.out.println("Error");
+			}
 		
+			con.commit();
 			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -88,15 +128,22 @@ public class EmployeeJDBC {
 		
 		try {
 		// 1 - Create a PreparedStatement with a query
-		
+			PreparedStatement pStmt = con.prepareStatement("select * employee where Salary = ?");
 
 		// 2 - Search for the given salary
-		
+			pStmt.setString(4, salary);
 
 		// 3 - Execute this query
+			ResultSet rs = pStmt.executeQuery();
 
-		
-		// 4 - While there are records, continue 
+		// 4 - While there are records, continue
+			if(rs.next()) {
+				emp = new Employee();
+				emp.setId(rs.getInt(1));
+				emp.setFirstName(rs.getString(2));
+				emp.setLastName(rs.getString(3));
+				emp.setSalary(rs.getInt(4));
+			}
 
 		con.close();
 		} catch (SQLException e) {
@@ -113,13 +160,24 @@ public class EmployeeJDBC {
 		
 		//1 - Create a PreparedStatement with a query "insert into employee values(?,?,?,?)" 
 		
-		con.setAutoCommit(false);
+//		con.setAutoCommit(false);
 
 		//	Substitute the ? now.
 		
 		//2 - Execute this query using executeUpdate()
+		con.setAutoCommit(false);
+		PreparedStatement statement = con.prepareStatement("INSERT INTO employee (firstname, lastname, salary) VALUES (?,?,?)");
+		statement.setString(1,emp.getFirstName());
+		statement.setString(2,emp.getLastName());
+		statement.setInt(3,emp.getSalary());
+		int result = statement.executeUpdate();
+		if(result==1) {
+			System.out.println("Successfully inserted values");
+		} else {
+			System.out.println("Error, something went wrong");
+		}
 			
-		System.out.println(rows + " row(s) added!");
+
 		con.commit();
 		con.close();
 		} catch (SQLException e) {
@@ -130,4 +188,4 @@ public class EmployeeJDBC {
 
 	}
 
-}
+
