25c92cdaa5c0170596df439a39c6d4df624534af
Marija Rodionova <marija.limare@gmail.com>
2021-03-05 11:12:28 +0200

    Final activity - Students

---
+++ SEF - Participants Workspace/SEF/src/sef/FinalActivity/src/Students.java
@@ -0,0 +1,15 @@
+public class Students {
+
+
+
+    public static void main(String[] args) {
+        String uni;
+
+        uni = "YALE";
+        System.out.println("I study in university " + uni);
+    }
+
+}
+
+
+
