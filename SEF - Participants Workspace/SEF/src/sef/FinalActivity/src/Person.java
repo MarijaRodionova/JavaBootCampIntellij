55360a4bba8005f95e093dc9f2692c20ce7dd579
Marija Rodionova <marija.limare@gmail.com>
2021-03-05 11:11:38 +0200

    Final activity - Person

---
+++ SEF - Participants Workspace/SEF/src/sef/FinalActivity/src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+
+    public static void main(String[] args) {
+
+    String name = "Benedict";
+    int age = 9;
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+    }
+
+
+}
