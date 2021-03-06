92778c7537d8ef4884e713ea670bf92481f28a81
Marija Rodionova <marija.limare@gmail.com>
2021-03-06 15:40:44 +0200

    activity

---
+++ SEF - Participants Workspace/SEF/src/sef/module6/sef/module6/activity/AbstractionActivity.java
@@ -0,0 +1,16 @@
+package sef.module6.activity;
+
+public class AbstractionActivity {
+
+    public static void main(String[] args) {
+
+        Rectangle r = new Rectangle();
+
+        r.getCalculateArea();
+        r.getCalculatePerimeter();
+
+        System.out.println();
+
+
+    }
+}
