bc03dbf3798f0114fb0e925f20ff6dc4afde9d93
Marija Rodionova <marija.limare@gmail.com>
2021-03-06 19:02:08 +0200

    Student final

--- SEF - Participants Workspace/SEF/src/sef/FinalActivity/src/Students.java
+++ SEF - Participants Workspace/SEF/src/sef/FinalActivity/src/Students.java
@@ -1,15 +1,17 @@
 public class Students {
 
-
-
     public static void main(String[] args) {
+        String nameOfStudent;
         String uni;
 
+        nameOfStudent = "ALex";
         uni = "YALE";
-        System.out.println("I study in university " + uni);
+        System.out.println("My name is " + nameOfStudent + " and i study in university " + uni);
     }
-
 }
+
+
+
 
 
 
